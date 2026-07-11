330c0f111ee89196f114c1e8034561e4a4a5a33e
himanshu979347 <himanshunayak2102@gmail.com>
2026-07-11 12:31:02 +0530

    Connect repository input with React state

--- client/src/components/RepositoryInput.jsx
+++ client/src/components/RepositoryInput.jsx
@@ -1,6 +1,8 @@
 import Button from "./Button";
+import { useState } from "react";
 
 function RepositoryInput() {
+    const[repoUrl, setrepoUrl]=useState("");
     return (
         <section className="py-20 bg-white">
 
@@ -9,7 +11,7 @@ function RepositoryInput() {
             </h2>
 
             <p className="text-center text-gray-600 mt-4">
-                Paste your GitHub repository URL and let DevLens AI explain your project.
+                Paste your GitHub repository URL and let DevLensAI explain your project.
             </p>
 
             <div className="max-w-3xl mx-auto mt-10 flex gap-4">
@@ -17,12 +19,17 @@ function RepositoryInput() {
                 <input
                     type="text"
                     placeholder="https://github.com/username/repository"
+                    value={repoUrl}
+                    onChange={(e) => setrepoUrl(e.target.value)}
                     className="flex-1 border border-gray-300 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-blue-500"
                 />
 
                 <Button text="Analyze" />
-
             </div>
+            <p className="mt-4 text-center text-blue-600">
+                    {repoUrl}
+                </p>
+
 
         </section>
     );
--- client/src/components/WhyChoose.jsx
+++ client/src/components/WhyChoose.jsx
@@ -2,7 +2,7 @@ function WhyChoose(){
     return(
         <section className="py-20 bg-slate-50">
             <h1 className="text-4xl font-bold text-center">
-                Why DevLens AI?
+                Why DevLensAI?
             </h1>
 
             <p className="text-center mt-4 text-gray-600 max-w-2xl mx-auto">
