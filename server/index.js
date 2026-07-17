e31a03dde97e818d7c5a535ca71f81b7c58eba7e
himanshu979347 <himanshunayak2102@gmail.com>
2026-07-17 23:10:11 +0530

    Create analyze API endpoint in Express

--- server/index.js
+++ server/index.js
@@ -2,9 +2,24 @@ const express= require("express");
 const app= express();
 const PORT=5000;
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+app.get("/", (req, res) => {
     res.send("DevLens AI Backend Running 🚀");
 });
+
+app.post("analyze",(req, res) => {
+    const {repoUrl}= req.body;
+    console.log(repoUrl);
+
+    res.json({
+        success: true,
+        message: "Repository received successfully",
+        repository: repoUrl
+    })
+
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
