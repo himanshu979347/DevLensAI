f6f5b56103de7e749294601a0b96e2584c09a549
himanshu979347 <himanshunayak2102@gmail.com>
2026-07-22 19:38:57 +0530

    Connect frontend with deployed backend

--- client/src/components/Analysis/RepositoryInput.jsx
+++ client/src/components/Analysis/RepositoryInput.jsx
@@ -32,7 +32,7 @@ function RepositoryInput() {
         }, 2500);
 
     try {
-        const response = await fetch("http://localhost:5000/analyze",{
+        const response = await fetch("https://devlensai.onrender.com/analyze",{
             method:"POST",
             headers:{"Content-Type":"application/json"},
             body:JSON.stringify({repoUrl})
